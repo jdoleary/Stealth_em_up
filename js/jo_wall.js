23e169d13d4da93b12c2f23b5904d8b0479b9b26
Jordan OLeary <jdoleary@gmail.com>
2014-06-13 21:25:33 -0400

    Added grid of walls.  Added images for grid.  There is a bug at 320,320 px which I'm colliding with for some reason.

--- js/jo_wall.js
+++ js/jo_wall.js
@@ -1,4 +1,5 @@
-function jo_wall(v8,v2,v4,v6){
+//also referred to as "cell"
+function jo_wall(image,solid,vertices){
     /*
     To be able to build walls dynamically they will be made of points
     Grid cells around the cell that the wall is in will only check against 
@@ -9,14 +10,24 @@ function jo_wall(v8,v2,v4,v6){
     For example, cell 2 will only need to check against vertex 2
     
     */
-    this.v2 = v2;
-    this.v4 = v4;
-    this.v6 = v6;
-    this.v8 = v8;
-    if(v2.x < 0 || v2.y < 0) throw new Error("Walls cannot be placed in negative space!");
-    if(v4.x < 0 || v4.y < 0) throw new Error("Walls cannot be placed in negative space!");
-    if(v6.x < 0 || v6.y < 0) throw new Error("Walls cannot be placed in negative space!");
-    if(v8.x < 0 || v8.y < 0) throw new Error("Walls cannot be placed in negative space!");
+    //yes, v8 is index 0 because I normally think of corner order in 8246 based on
+    //the above illustration
+    this.solid = solid;//if true, hero cannot walk through the wall
+    
+    this.v8 = vertices[0];
+    this.v2 = vertices[1];
+    this.v4 = vertices[2];
+    this.v6 = vertices[3];
+    if(this.v2.x < 0 || this.v2.y < 0) throw new Error("Walls cannot be placed in negative space!");
+    if(this.v4.x < 0 || this.v4.y < 0) throw new Error("Walls cannot be placed in negative space!");
+    if(this.v6.x < 0 || this.v6.y < 0) throw new Error("Walls cannot be placed in negative space!");
+    if(this.v8.x < 0 || this.v8.y < 0) throw new Error("Walls cannot be placed in negative space!");
+    
+    //for drawing image
+    this.image_sprite = new PIXI.Sprite(image);
+    this.x = this.v8.x;
+    this.y = this.v8.y;
+    stage.addChild(this.image_sprite);
     
     
     this.graphics = new PIXI.Graphics();
@@ -37,6 +48,11 @@ function jo_wall(v8,v2,v4,v6){
 		this.graphics.lineTo(v8_ob.x, v8_ob.y);
 		this.graphics.lineTo(v2_ob.x, v2_ob.y);
     
+    };
+    this.prepare_for_draw = function(){
+        var draw_coords = camera.relativePoint(this);
+        this.image_sprite.position.x = draw_coords.x;
+        this.image_sprite.position.y = draw_coords.y;
     };
     
 }
